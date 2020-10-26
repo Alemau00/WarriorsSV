31593736a607c55fe06021eb183f4259d8767e90
Alemau00 <68650342+Alemau00@users.noreply.github.com>
2020-10-25 19:11:44 -0600

    RESPONSIVE FALLIDO

--- Negocio Audífonos/js/Desplegable.js
+++ Negocio Audífonos/js/Desplegable.js
@@ -1,27 +1,9 @@
 $(document).ready(main);
-var contador=1;
+var contador = 1;
 
 function main()
 {
-    $('.desplegable').click(animacion);
-    function animacion()
-    {
-        if(contador==1)
-        {
-            $('#head-right').animate({
-                left:'0'
-            });
-            contador=0;
-        }
-        else
-        {
-            contador=1;
-            $('#head-right').animate({
-                left:'-100%'
-            });
-        }
-    }
-
+    //Submenus
     $('.submenu').click(function(){
         $(this).children('.secondlist').slideToggle();
     })
