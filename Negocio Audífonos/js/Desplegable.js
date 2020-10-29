5b1b84cd64d281d3d05e15fbac69066daef3ecb5
Alemau00 <68650342+Alemau00@users.noreply.github.com>
2020-10-28 21:18:57 -0600

    ARREGLO RESPONSIVE

--- Negocio Audífonos/js/Desplegable.js
+++ Negocio Audífonos/js/Desplegable.js
@@ -1,10 +1,5 @@
-$(document).ready(main);
-var contador = 1;
-
-function main()
-{
-    //Submenus
-    $('.submenu').click(function(){
-        $(this).children('.secondlist').slideToggle();
-    })
-}
+$(function(){
+    $('#desple').click(function(){
+        $('#desple').slideToggle("slow");
+    });
+});
